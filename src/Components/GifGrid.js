b9c6acd5843e598c23da5b90f466bdb1a13aa9b4
Angel Chavez <angelchaez.325.ac@gmail.com>
2020-09-15 21:46:26 -0600

    segunda actualizacion

--- src/Components/GifGrid.js
+++ src/Components/GifGrid.js
@@ -3,26 +3,26 @@ import { getGif } from '../helpers/getGifs';
 import { useFetchGifs } from '../hooks/useFetchGifs';
 import Giffitem from './Giffitem';
 
-export const GifGrid = ({categoria}) => {
-    const {data, loading} = useFetchGifs(categoria);
+export const GifGrid = ({ categoria }) => {
+    const { data, loading } = useFetchGifs(categoria);
     console.log(loading);
-   /* const [images, setimg] = useState([]);
-    useEffect(() => {
-        getGif(categoria)
-            .then(setimg);
-    }, [categoria])*/
+    /* const [images, setimg] = useState([]);
+     useEffect(() => {
+         getGif(categoria)
+             .then(setimg);
+     }, [categoria])*/
     return (
-        <div className = "card-grid">
-            <h3>{loading? "Carando...." : ""}</h3>
-                {
-                    data.map((img)=>(
+        <div className="card-grid">
+            <h3>{loading ? "Carando...." : ""}</h3>
+            {
+                data.map((img) => (
                     <Giffitem
-                        key = {img.id}
+                        key={img.id}
                         {...img}
                     />
-                    ))
-                }
-            
+                ))
+            }
+
         </div>
     )
 }
